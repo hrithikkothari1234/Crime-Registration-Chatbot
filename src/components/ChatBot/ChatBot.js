5b3563151bc7a1a3e4db2d004193e7948b041bad
hritikkothari <kotharihrithik1@gmail.com>
2020-11-30 23:02:49 +0530

    updated readme

---
+++ src/components/ChatBot/ChatBot.js
@@ -0,0 +1,6 @@
+<iframe
+    allow="microphone;"
+    width="350"
+    height="430"
+    src="https://console.dialogflow.com/api-client/demo/embedded/1ccc3f6f-ddb6-431e-8241-1dc94a134687">
+</iframe>
