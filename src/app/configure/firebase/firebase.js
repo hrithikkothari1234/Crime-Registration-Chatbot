e6e77889e4d71c32143162c62fce0fd813263838
hritikkothari <kotharihrithik1@gmail.com>
2020-12-02 03:16:06 +0530

    nvm

--- src/app/configure/firebase/firebase.js
+++ src/app/configure/firebase/firebase.js
@@ -6,7 +6,14 @@ import 'firebase/storage'
 
 
 const firebaseConfig = {
-    //config
+    apiKey: "AIzaSyAke41zbOzUOpRmHFzz_LCgjah3f6XSd-Y",
+    authDomain: "chatbot-700f4.firebaseapp.com",
+    databaseURL: "https://chatbot-700f4.firebaseio.com",
+    projectId: "chatbot-700f4",
+    storageBucket: "chatbot-700f4.appspot.com",
+    messagingSenderId: "839358854890",
+    appId: "1:839358854890:web:efc10f339b0b488a164993",
+    measurementId: "G-7CMTMEZ4RV"
 };
 
 firebase.initializeApp(firebaseConfig)
