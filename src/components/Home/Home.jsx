2b5b0fffe2f85d04fc1a1a03b072be4a77d87c07
Dhrumil Shah <dhrumilshah0310@gmail.com>
2020-12-01 14:00:39 +0530

    small changes

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -42,7 +42,7 @@ class Home extends React.Component {
 					<Container className="myContainer animated animatedFadeInUp fadeInUp">
 						<h5>Crime Registration &amp; Crime Awareness System</h5>
 						<p>
-							Our aim to eradicate crime from the system.If you have any queries or want to register a case click on the red chatbot option.
+							Our aim to eradicate crime from the world. If you have any queries or want to register a case click on the red chatbot option.
 						</p>
 					</Container>
 				</div>
