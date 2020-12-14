f7180c5159306d1bef3372535327d38b8c2dc612
hritikkothari <kotharihrithik1@gmail.com>
2020-12-14 21:55:32 +0530

    updated firebase - removed keys

---
+++ src/app/configure/firebase/firebase.js
@@ -0,0 +1,34 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+import 'firebase/database'
+import 'firebase/storage'
+
+
+const firebaseConfig = {
+  // keys
+};
+
+firebase.initializeApp(firebaseConfig)
+firebase.firestore()
+export default firebase
+
+export const db = firebase.firestore();
+
+export const cases_registered = async() =>{
+  const data = []
+  const events = await db.collection('Cases').get()
+    .then(querySnapshot => {
+      querySnapshot.docs.map(doc => {
+        data.push([doc.data(),doc.id])
+      });
+    });
+  return data
+}
+
+export const DeleteCase = async(Id) =>{
+  return db.collection('Cases').doc(Id).delete()
+}
+
+
+
